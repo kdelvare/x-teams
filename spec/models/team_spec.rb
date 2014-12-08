744a4a77f53e21be25258f46a14a8131cecf6acc
Karine Delvare <kdelvare@gmail.com>
2014-12-08 07:02:00 +0100

    Team model specs

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -12,12 +12,63 @@ RSpec.describe Team, :type => :model do
     end
   end
 
+  describe "#active?" do
+    context "with active team" do
+      it "should return true" do
+        team = Team.create
+        expect(team.active?).to be_truthy
+      end
+    end
+
+    context "with disbanded team" do
+      it "should return false" do
+        team = Team.create
+        team.disband
+        expect(team.active?).to be_falsey
+      end
+    end
+  end
+
+  describe "#disbanded?" do
+    context "with active team" do
+      it "should return false" do
+        team = Team.create
+        expect(team.disbanded?).to be_falsey
+      end
+    end
+
+    context "with disbanded team" do
+      it "should return true" do
+        team = Team.create
+        team.disband
+        expect(team.disbanded?).to be_truthy
+      end
+    end
+  end
+
+  describe "#disband" do
+    it "should set disbanded_at" do
+      team = Team.create
+      team.disband
+      expect(team.disbanded_at).to be
+    end
+  end
+
   describe "active scope" do
     it "should return only active teams" do
       team1 = Team.create
       team2 = Team.create
       team1.disband
       expect(Team.active).to eq([team2])
+    end
+  end
+
+  describe "disbanded scope" do
+    it "should return only disbanded teams" do
+      team1 = Team.create
+      team2 = Team.create
+      team1.disband
+      expect(Team.disbanded).to eq([team1])
     end
   end
 end
